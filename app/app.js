27804a39d7c3864475a3b879143f4d5409ec5889
Lennex Zinyando <brizdigital@gmail.com>
2023-04-28 10:41:30 +0200

    Install and configure tailwindcss

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'surrealdb-crm/config/environment';
+import 'surrealdb-crm/app.css';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
